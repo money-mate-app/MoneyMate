10de5820c18a7ca235cae7dbf8cd0c50f8bcb513
Zaid <zaidodam@gmai.com>
2023-07-16 23:06:38 +0530

    ref: update validation

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -6,12 +6,12 @@ import { z } from "zod";
 const envSchema = z.object({
   DATABASE_URL: z.string({
     required_error: "Database url is required",
-  }),
+  }).min(10, "Please provide a valid Database URL"),
 
   PORT: z.number().optional().default(5000),
   REDIS_URL: z.string({
     required_error: "Redis url is required",
-  }),
+  }).min(10, "Please provide a valid Redis URL"),
 });
 
 export const __dev__ = process.env.NODE_ENV !== "production";
