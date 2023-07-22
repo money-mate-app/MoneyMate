5ee32057cd57a62fd715f52851dbcaed41eb958a
vinay2812 <vinaysarda2812@gmail.com>
2023-07-23 02:11:50 +0530

    feat: add logos and other utilities

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,4 +1,5 @@
 import { app } from "../connections";
+import AuthRoute from "./auth"
 
 app.get("/", (req, res, next) => {
   try {
@@ -9,9 +10,11 @@ app.get("/", (req, res, next) => {
       server: status,
       version,
     });
-  } catch (err) {
+  } catch (err: any) {
     res.status(500).json({
       server: "inactive",
     });
   }
 });
+
+app.use("/auth", AuthRoute)
