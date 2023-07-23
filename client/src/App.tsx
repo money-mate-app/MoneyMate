1907956e2b8637c55a0f72a5867a63938d2be68a
vinay2812 <vinaysarda2812@gmail.com>
2023-07-24 01:04:47 +0530

    ref: update route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
+import { Route, Routes } from "react-router-dom";
 import { lazyImport } from "./lib/utils";
 import Providers from "./providers";
+import { Auth } from "./modules/auth";
 
 const { AuthRoutes } = lazyImport(
   () => import("@/modules/auth/routes"),
@@ -8,7 +10,10 @@ const { AuthRoutes } = lazyImport(
 const App = () => {
   return (
     <Providers>
-      <AuthRoutes />
+      {/* <AuthRoutes /> */}
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
     </Providers>
   );
 };
